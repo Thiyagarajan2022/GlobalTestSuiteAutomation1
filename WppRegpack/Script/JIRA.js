6e53ecc42a0e75dde0effb4fa47ea8e0631522f3
rajeev-wpp <rajeevdaniel.isaac@cognizant.com>
2020-06-10 10:08:23 +0530

    JIRA updated code

--- WppRegpack/Script/JIRA.js
+++ WppRegpack/Script/JIRA.js
@@ -9,13 +9,17 @@
 function JIRAUpdate(folderName,testCaseId,versionName,cycleName){
 
      projectName = EnvParams.Pname;
+     	var userName = EnvParams.JiraUsername;
+		var  accessKey = EnvParams.JiraAccessKey;
+		var secretKey = EnvParams.JiraSecrekey;
+    var zephyrBaseUrl =EnvParams.JirazephyrBaseUrl;
 //		 versionName = EnvParams.Vname;
 //		 cycleName = EnvParams.Cname;
 //     cycleName = EnvParams.Country;
 //		var userName = "muthukumar.m@cognizant.com";
 //		var  accessKey = "MDA1MDIyZWQtZmEyMC0zOTc4LWI2ZmEtZDM3MTcxMGU1YzRjIDVjYjc1OTJmOWE4NTc5MTA4OTZmZTc5OSBVU0VSX0RFRkFVTFRfTkFNRQ";
 //		var secretKey = "jf9LV-GHNp6MKw35xCTPo43WC0V4bwYC4SdsZC5K-Ho"; 
- //   var zephyrBaseUrl ="https://prod-api.zephyr4jiracloud.com/connect";
+//    var zephyrBaseUrl ="https://prod-api.zephyr4jiracloud.com/connect";
     var entityName = "execution";
     var expirationInsec = 360;
     var comment = "TestReport_Uploaded_Successfully_in_JIRA.";
