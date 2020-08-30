9f2ff3f72206b88ef1cc81decf4f4badf8b8a4d0
MuthuKumarMurugesan <muthukumar.m@cognizant.com>
2020-08-30 17:18:48 +0530

    Updating GL Module

--- WppRegpack/Script/PrintPaymentRemittance.js
+++ WppRegpack/Script/PrintPaymentRemittance.js
@@ -437,8 +437,16 @@ Log.Error("Could not create the folder " + sFolder);
 save.Keys(sFolder+SaveTitle+".pdf");
 
 filepathforMplValidation =sFolder+SaveTitle+".pdf";
-var saveAs = Sys.Process("AcroRd32").Window("#32770", "Save As", 1).Window("Button", "&Save", 1);
+//var saveAs = Sys.Process("AcroRd32").Window("#32770", "Save As", 1).Window("Button", "&Save", 1);
+//saveAs.Click();
+var p = Sys.Process("AcroRd32").Window("#32770", "Save As", 1);
+Sys.HighlightObject(p);
+var saveAs = p.FindChild("WndCaption", "&Save", 2000);
+if (saveAs.Exists)
+{ 
 saveAs.Click();
+}
+aqUtils.Delay(2000, Indicator.Text);
 aqUtils.Delay(2000, Indicator.Text);
 //if(ImageRepository.ImageSet.SaveAs.Exists()){
 //var conSaveAs = Sys.Process("AcroRd32").Window("#32770", "Confirm Save As", 1).UIAObject("Confirm_Save_As").Window("CtrlNotifySink", "", 7).Window("Button", "&Yes", 1)
