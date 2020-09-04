35c75ad17279477c09db683a658f0292e174f094
MuthukumarMurugesan <Muthukumar.M@cognizant.com>
2020-09-04 14:54:55 +0530

    Updating Human Resourse Image for spanish and Chinese

--- WppRegpack/Script/FixedAssetDisposal.js
+++ WppRegpack/Script/FixedAssetDisposal.js
@@ -377,7 +377,7 @@ Sys.HighlightObject(table);
     }
   }
 
-  
+  Log.Message(transactionNo)
   
 } 
 
@@ -624,8 +624,16 @@ Log.Error("Could not create the folder " + sFolder);
 save.Keys(sFolder+SaveTitle+".pdf");
 
 var filepathforMplValidation =sFolder+SaveTitle+".pdf";
-var saveAs = Sys.Process("AcroRd32").Window("#32770", "Save As", 1).Window("Button", "&Save", 1);
+//var saveAs = Sys.Process("AcroRd32").Window("#32770", "Save As", 1).Window("Button", "&Save", 1);
+//saveAs.Click();
+var p = Sys.Process("AcroRd32").Window("#32770", "Save As", 1);
+Sys.HighlightObject(p);
+var saveAs = p.FindChild("WndCaption", "&Save", 2000);
+if (saveAs.Exists)
+{ 
 saveAs.Click();
+}
+aqUtils.Delay(2000, Indicator.Text);
 aqUtils.Delay(2000, Indicator.Text);
 
 Sys.HighlightObject(pdf);
