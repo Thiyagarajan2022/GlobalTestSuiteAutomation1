9b64f7f157aa02ea21ec9f648157762859ab175c
MuthuKumarMurugesan <muthukumar.m@cognizant.com>
2022-02-11 19:37:32 +0530

    Removing Folders

--- SmokeSuite/MultiLingualTranslator/Translate.js
+++
@@ -1,6 +0,0 @@
-const translate = require('google-translate-api');
-translate('Purchase Order No', {to: 'en'}).then(res => {
-  console.log(res.text);
-}).catch(err => {
-   console.error(err);
-});
